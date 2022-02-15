e42cc98d9b845e49a7be29a2c51d0d4e41f565c1
Jason Whisnant <tasups@gmail.com>
2022-02-15 16:39:56 +0000

    add ejs

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -1,8 +1,34 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+app.set("view engine", "ejs");
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'join_us'
+});
 
 app.get("/", function(req, res){
- res.send("HELLO FROM OUR WEB APP!");
+	const q = 'SELECT COUNT(*) AS count FROM users';
+	connection.query(q, function(err, result){
+		if (err) throw err;
+		const count = result[0].count;
+		res.render("home", {count: count});
+	  //res.send(`We have ${count} users in the database.`);
+	})
+});
+
+app.get("/joke", function(req, res){
+	const joke = "What do you call a dog that can do magic tricks? A Labracadabrador!";
+	res.send(joke);
+})
+
+app.get("/random_num", function(req, res){
+  let num = Math.floor(Math.random() * 10) + 1;
+	console.log(num);
+  res.send(`Your lucky number is ${num}!`);
 });
 
 app.listen(3000, function () {
